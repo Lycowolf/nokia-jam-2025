15bebdf9000268e71e5125be76fff2f6769689d4
Lyco <lyco@lyco.name>
2025-02-23 23:40:41 +0100

    update hints, clean up

--- the-grind-2025/ui.py
+++ the-grind-2025/ui.py
@@ -5,13 +5,13 @@ import re
 WORD_RE = re.compile(r"([^ \uE020]+)([ \uE020]*)") # E020 is inverted space
 font = pyxel.Font(FONT)
 
-def switch_palette(name: str):
+def switch_palette(name: str) -> None:
     """switch palette. If it doesn't exist, do nothing"""
     if name in PALETTES:
         # load only palette; the pyxel python binding exposes wrong type
         pyxel.load(f"assets/palettes/{name}.pyxres", excl_images=True, excl_musics=True, excl_sounds=True, excl_tilemaps=True)
 
-def draw_text_row(row, text: str, color: int = 1, x_off: int = 0):
+def draw_text_row(row, text: str, color: int = 1, x_off: int = 0) -> None:
     """
     Text-printing primitive: prints a line of text to a specified screen row.
     Rows outside the allowed area are ignored.
@@ -19,12 +19,12 @@ def draw_text_row(row, text: str, color: int = 1, x_off: int = 0):
     if 0 <= row < TEXT_ROWS:
         pyxel.text(x_off, row * FONT_HEIGHT + TEXT_OFFSET_Y, text, color, font)
 
-def check_word(word: str, known_words: set[str]):
+def check_word(word: str, known_words: set[str]) -> str:
     if word not in known_words:
         print(f"Word {word} not in the set of known words {known_words}")
     return word
 
-def invert_text_blink(text, uninvert_selected):
+def invert_text_blink(text, uninvert_selected) -> str:
     if uninvert_selected:
         return text
     out_text = ""
@@ -76,46 +76,3 @@ def draw_smart_text(text: str, words: list[str], known_words: set[str], selected
     if is_smart:
         word_rows.append(row_num)
     return word_rows
-
-"""
-def draw_wrapped_text(text: str, starting_line: int = 0):
-    row_num = - starting_line
-    row_text = ""
-
-    for match in re.finditer(WORD_RE, text):
-        next_word = match[0]
-        try_text = row_text + next_word
-        if font.text_width(try_text) > SCREEN_W:
-            draw_text_row(row_num, row_text)
-            row_num += 1
-            row_text = next_word
-        else:
-            row_text = try_text
-    draw_text_row(row_num, row_text)
-
-def prepare_smart_text(text: str, words: list[str], known_words: set[str], selected: int, blink_uninvert: bool = False) -> str:
-    static_segments = text.split(SMART_TEXT_MARKER)
-    last_segment = len(static_segments) - 1
-
-    if last_segment != len(words):
-        print(f"number of placeholders in '{text}' doesn't match the number of words in {words}")
-
-    if last_segment == 0:
-        return text
-
-    out_text = ""
-    for i, segment in enumerate(static_segments):
-        out_text += segment
-        if i == last_segment:
-            return out_text
-
-        try:
-            replacement = check_word(words[i], known_words)
-        except IndexError:
-            print(f"No word for index {i}")
-            replacement = "-noidx-"
-        if i == selected and blink_uninvert:
-            out_text += replacement
-        else:
-            out_text += invert_text_blink(replacement)
-"""
