7e5b105fc46b357fa75eda01428a0bf4a16377a7
Martin Dörfler <tryi.dent@gmail.com>
2025-02-24 21:43:15 +0100

    Set palette in main

--- the-grind-2025/main.py
+++ the-grind-2025/main.py
@@ -4,6 +4,7 @@ import sound
 from constants import *
 import screens
 import scenario.investigation_test as test_investigation
+from ui import switch_palette
 
 class Game:
     screen: screens.Screen
@@ -11,6 +12,8 @@ class Game:
     def __init__(self):
         pyxel.init(SCREEN_W, SCREEN_H, fps=FPS, title="Nokia 3310 Jam 2025", display_scale=DISPLAY_SCALE)
         sound.init()
+
+        switch_palette('gray')
 
         self.screen = test_investigation.setup_scenario(skip_intro=False)
         # self.screen = screens.Menu()
