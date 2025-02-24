392180594e8e6939ec3eafef52e81aa22c5dfef8
Martin Dörfler <tryi.dent@gmail.com>
2025-02-24 23:28:10 +0100

    Add proper base type for Screen

--- the-grind-2025/screen/base.py
+++ the-grind-2025/screen/base.py
@@ -1,6 +1,7 @@
+from abc import ABC
 from typing import Self
 
-class Screen:
+class Screen(ABC):
     # Screens form a state machine. update() method updates self as necessary and returns a Screen to be displayed
     # (which can be self).
     def update(self) -> Self:
